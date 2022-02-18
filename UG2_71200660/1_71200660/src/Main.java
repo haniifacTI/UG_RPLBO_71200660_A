f14c297e98e81ee885113a26790999f0abb98754
Haniif Ahmad C <78332738+haniifacTI@users.noreply.github.com>
2022-02-18 08:06:29 +0700

    Nomer 1 selesai

---
+++ UG2_71200660/1_71200660/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        TabunganKonvensional K_71200660 = new TabunganKonvensional();
+        TabunganBerjangka I_71200660 = new TabunganBerjangka();
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Pilihan:");
+        System.out.println("1. Penyetoran");
+        System.out.println("2. Penarikan");
+        System.out.println("3. Lihat Saldo");
+        System.out.println("Masukkan Pilihan:");
+        int pilihan = userInput.nextInt();
+
+        if (pilihan == 1){
+            K_71200660.penyetoran(200000);
+            I_71200660.penyetoran(1000000);
+
+        }else if (pilihan == 2){
+            K_71200660.penarikan(50000);
+            I_71200660.penarikan(0);
+
+        }else if (pilihan == 3){
+            System.out.print("Saldo K_71200660 Rp.");
+            K_71200660.getSaldo();
+            System.out.print("Saldo I_71200660 Rp.");
+            I_71200660.getSaldo();
+        }
+    }
+}
